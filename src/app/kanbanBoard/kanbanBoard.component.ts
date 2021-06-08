530085a4ec3ffe79362dc3a9ebf26b7680b6fcdd
Luis Alberto de la Cruz Medina <delacruzmedina@zenkotec.com.mx>
2021-06-08 10:53:21 -0500

    Actualización

--- src/app/kanbanBoard/kanbanBoard.component.ts
+++ src/app/kanbanBoard/kanbanBoard.component.ts
@@ -10,9 +10,12 @@ export class KanbanBoard implements OnInit {
   stagesNames: string[];
   stagesTasks: any[]; //Only used for rendering purpose
 
+  task: string;
+
   ngOnInit() {
-    // Each task is uniquely identified by its name. 
+    // Each task is uniquely identified by its name.
     // Therefore, when you perform any operation on tasks, make sure you pick tasks by names (primary key) instead of any kind of index or any other attribute.
+    this.task = '';
     this.tasks = [
       { name: '0', stage: 0 },
       { name: '1', stage: 0 },
@@ -20,7 +23,7 @@ export class KanbanBoard implements OnInit {
     this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
     this.configureTasksForRendering();
   }
-  
+
   // this function has to be called whenever tasks array is changed to construct stagesTasks for rendering purpose
   configureTasksForRendering = () => {
     this.stagesTasks = [];
@@ -35,6 +38,39 @@ export class KanbanBoard implements OnInit {
 
   generateTestId = (name) => {
     return name.split(' ').join('-');
+  }
+
+  onCreateTask() {
+    if(this.task !== ''){
+      this.tasks.push({name: this.task, stage: 0});
+      this.configureTasksForRendering();
+    }
+
+    this.task = '';
+    console.log(this.task);
+  }
+
+  nextStage(task: any) {
+    console.log(task);
+    task.stage = task.stage+1;
+    this.configureTasksForRendering();
+    console.log(task);
+  }
+
+  backStage(task: any) {
+    task.stage = task.stage - 1;
+    this.configureTasksForRendering();
+  }
+
+  delete(task: any) {
+
+    const taskIndex = this.tasks.indexOf(task, 0);
+
+    if(taskIndex > -1){
+      this.tasks.splice(taskIndex, 1);
+    }
+
+    this.configureTasksForRendering();
   }
 }
 
